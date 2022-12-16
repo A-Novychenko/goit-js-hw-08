7cf1245bd28bb7b2c7e8789c3739f3f3f6d7cfff
Aleksandr Novychenko <A.Novychenko@outlook.com>
2022-12-16 12:08:31 +0200

    add vimeoplayer(init ant start of current time)

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', function (data) {
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(data.seconds)
+  );
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+const seconds = Number(localStorage.getItem('videoplayer-current-time'));
+player.setCurrentTime(seconds);
+//   .then(function (seconds) {})
+//   .catch(function (error) {
+//     switch (error.name) {
+//       case 'RangeError':
+//         // the time was less than 0 or greater than the video’s duration
+//         break;
+
+//       default:
+//         // some other error occurred
+//         break;
+//     }
+//   });
