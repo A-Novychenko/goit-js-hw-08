42503639dc1c483ce1648740965a19ce603fc7f0
Aleksandr Novychenko <A.Novychenko@outlook.com>
2022-12-16 14:38:12 +0200

    upd

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,16 +1,21 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
 
-player.on('timeupdate', function (data) {
-  localStorage.setItem(
-    'videoplayer-current-time',
-    JSON.stringify(data.seconds)
-  );
-});
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    localStorage.setItem(
+      'videoplayer-current-time',
+      JSON.stringify(data.seconds)
+    );
+  }, 1000)
+);
 
 player.setCurrentTime(Number(localStorage.getItem('videoplayer-current-time')));
+
 //   .then(function (seconds) {
 //     console.log('player ARBITTEN!');
 //   })
