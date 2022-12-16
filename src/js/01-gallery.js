284b42b549efaaa5777fc5a1797bcc4ede0009ad
Aleksandr Novychenko <A.Novychenko@outlook.com>
2022-12-16 02:36:12 +0200

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import { galleryItems } from './gallery-items.js';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryCardsMarkup = createGalleryCardsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryCardsMarkup);
+
+function createGalleryCardsMarkup(galleryItems) {
+  return galleryItems.reduce((acc, { preview, original, description }) => {
+    return (acc += ` <a class="gallery__item" href="${original}">
+                  <img
+                       class="gallery__image"
+                       src="${preview}"
+                       alt="${description}"
+                   />
+                </a>`);
+  }, '');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  alertError: false,
+});
