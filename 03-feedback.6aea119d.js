4b8ae6948f7737af6db28beb400c58d2ce6cd0bb
A-Novychenko <A.Novychenko@outlook.com>
2022-12-16 17:45:48 +0000

    Deploying to gh-pages from @ A-Novychenko/goit-js-hw-08@9e16b02a38cef4550fcc1e04891b1fb12c392e02 🚀

--- 03-feedback.6aea119d.js
+++
@@ -1,2 +0,0 @@
-document.body.style.backgroundColor="black",document.querySelector(".feedback-form").addEventListener("input",(function(){}));
-//# sourceMappingURL=03-feedback.6aea119d.js.map
--- 03-feedback.8a617f3f.js
+++
@@ -1,2 +0,0 @@
-document.body.style.backgroundColor="black";document.querySelector(".feedback-form").addEventListener("input",(function(){}));
-//# sourceMappingURL=03-feedback.8a617f3f.js.map
