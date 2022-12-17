0f6c26ccc7b8f730da6f6a80474110ae3068f4a8
Aleksandr Novychenko <A.Novychenko@outlook.com>
2022-12-17 12:44:58 +0200

    complited task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,20 +1,3 @@
-// Відстежуй на формі подію input, і щоразу записуй у локальне сховище
-// об'єкт з полями email і message,
-// у яких зберігай поточні значення полів форми.
-// Нехай ключем для сховища буде рядок "feedback-form-state".
-
-// Під час завантаження сторінки перевіряй стан сховища,
-// і якщо там є збережені дані, заповнюй ними поля форми.
-// В іншому випадку поля повинні бути порожніми.
-
-// Під час сабміту форми очищуй сховище і поля форми,
-// а також виводь у консоль об'єкт з полями email, message та їхніми поточними значеннями.
-
-// Зроби так, щоб сховище оновлювалось не частіше, ніж раз на 500 мілісекунд.
-// Для цього додай до проекту і використовуй бібліотеку lodash.throttle.
-document.body.style.backgroundColor = 'black'; //
-////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////
 import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
@@ -22,14 +5,12 @@ const inputMail = document.querySelector('.feedback-form input');
 const inputText = document.querySelector('.feedback-form textarea');
 
 const STORAGE_KEY = 'feedback-form-state';
-let formData = {};
-// const formData = {};
-// розпарсені дані з локал сторадж або пустий обєкт
-const isThereDataInStorage = localStorage.getItem(STORAGE_KEY);
-// const localData = JSON.parse(isThereDataInStorage) ?? {};
-let localData = JSON.parse(isThereDataInStorage) || {};
+const formData = {};
 
-form.addEventListener('input', onFormInput);
+const isThereDataInStorage = localStorage.getItem(STORAGE_KEY);
+const localData = JSON.parse(isThereDataInStorage) || {};
+
+form.addEventListener('input', throttle(onFormInput, 500));
 form.addEventListener('submit', onFormSubmit);
 
 if (isThereDataInStorage) {
@@ -42,16 +23,23 @@ if (isThereDataInStorage) {
 }
 
 function onFormInput(e) {
-  formData = { ...localData, [e.target.name]: e.target.value };
   formData[e.target.name] = e.target.value;
 
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  localStorage.setItem(
+    STORAGE_KEY,
+    JSON.stringify({ ...localData, ...formData })
+  );
 }
 
 function onFormSubmit(e) {
   e.preventDefault();
+
+  const { email, message } = e.currentTarget.elements;
+  console.log({
+    email: email.value,
+    message: message.value,
+  });
+
   localStorage.removeItem(STORAGE_KEY);
-  e.target.reset();
-  localData = {};
-  //   console.log(localData);
+  e.currentTarget.reset();
 }
